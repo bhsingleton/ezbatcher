88aa73f3818b66b7552443bf4f1d491d647351f2
Ben Singleton <bhsingleton@gmail.com>
2023-05-21 09:00:28 -0400

    Adding fallback logic for scenes with no sequencers.

--- tasks/exportfbxtask.py
+++ tasks/exportfbxtask.py
@@ -1,7 +1,10 @@
 import os
 
-from dcc.fbx.libs import fbxio
-from dcc.perforce import cmds
+from dcc import fnreference
+from dcc.ui import qdirectoryedit
+from dcc.python import stringutils
+from dcc.perforce import cmds, p4utils
+from dcc.fbx.libs import fbxio, fbxsequencer, fbxsequence
 from .abstract import abstracttask
 
 import logging
@@ -16,7 +19,13 @@ class ExportFbxTask(abstracttask.AbstractTask):
     """
 
     # region Dunderscores
-    __slots__ = ('_fbxIO', '_animationOnly', '_checkout')
+    __slots__ = (
+        '_fbxIO',
+        '_animationOnly',
+        '_alternateDirectory',
+        '_checkout'
+    )
+
     __title__ = 'Export Fbx'
 
     def __init__(self, *args, **kwargs):
@@ -30,6 +39,7 @@ class ExportFbxTask(abstracttask.AbstractTask):
         #
         self._fbxIO = fbxio.FbxIO().weakReference()
         self._animationOnly = kwargs.get('animationOnly', True)
+        self._alternateDirectory = kwargs.get('alternateDirectory', '')
         self._checkout = kwargs.get('checkout', False)
 
         # Call parent method
@@ -70,6 +80,27 @@ class ExportFbxTask(abstracttask.AbstractTask):
         self._animationOnly = animationOnly
 
     @property
+    def alternateDirectory(self):
+        """
+        Getter method that returns the export directory override.
+
+        :rtype: str
+        """
+
+        return self._alternateDirectory
+
+    @alternateDirectory.setter
+    def alternateDirectory(self, alternateDirectory):
+        """
+        Setter method that updates the export directory override.
+
+        :type alternateDirectory: str
+        :rtype: None
+        """
+
+        self._alternateDirectory = os.path.normpath(alternateDirectory)
+
+    @property
     def checkout(self):
         """
         Getter method that returns the "checkout" flag.
@@ -92,10 +123,27 @@ class ExportFbxTask(abstracttask.AbstractTask):
     # endregion
 
     # region Methods
+    @classmethod
+    def createEditor(cls, name, parent=None):
+        """
+        Returns a Qt editor for the specified property.
+
+        :type name: str
+        :type parent: Union[QtWidgets.QWidget, None]
+        :rtype: Union[QtWidgets.QWidget, None]
+        """
+
+        if name == 'alternateDirectory':
+
+            return qdirectoryedit.QDirectoryEdit(parent=parent)
+
+        else:
+
+            return super(ExportFbxTask, cls).createEditor(name, parent=parent)
 
     def exportSets(self):
         """
-        Executes all the export sets inside the current scene file.
+        Exports all the export sets inside the current scene file.
 
         :rtype: None
         """
@@ -126,41 +174,89 @@ class ExportFbxTask(abstracttask.AbstractTask):
 
                 cmds.add(exportPath)
 
-    def exportSequences(self):
+    def exportReferencedAssets(self):
         """
-        Executes all the sequences inside the current scene file.
+        Tries to export any animation from referenced assets inside the current scene file.
+        This is done by checking if any of the reference nodes contains any export set data.
+        If no export set data is found then the referenced asset is ignored!
 
         :rtype: None
         """
 
-        # Iterate through sequencers
+        # Check if scene contains any references
+        #
+        references = list(fnreference.FnReference.iterSceneReferences())
+        numReferences = len(references)
+
+        if numReferences == 0:
+
+            log.warning('Scene contains no referenced assets!')
+            return
+
+        # Collect references from scene
+        #
+        reference = fnreference.FnReference()
+        reference.setQueue(references)
+
+        while not reference.isDone():
+
+            # Initialize sequencer
+            #
+            name = self.scene.currentName()
+            directory = self.alternateDirectory if not stringutils.isNullOrEmpty(self.alternateDirectory) else 'Export'
+            sequence = fbxsequence.FbxSequence(name=name, directory=directory, useTimeline=True)
+
+            guid = reference.guid()
+            sequencer = fbxsequencer.FbxSequencer(guid=guid, sequences=[sequence])
+
+            if not sequencer.isValid():
+
+                log.warning(f'Cannot locate valid asset from reference: {guid}')
+                reference.next()
+
+                continue
+
+            # Export sequence
+            #
+            exportPath = sequence.export(checkout=self.checkout)
+
+            # Go to next reference
+            #
+            reference.next()
+
+    def exportSequences(self):
+        """
+        Exports all the sequences inside the current scene file.
+
+        :rtype: None
+        """
+
+        # Check if file contains any sequencers
         #
         sequencers = self.fbxIO.loadSequencers()
+        numSequencers = len(sequencers)
 
+        if numSequencers == 0:
+
+            self.exportReferencedAssets()
+
+        # Iterate through sequencers
+        #
         for sequencer in sequencers:
 
             # Iterate through sequences
             #
             for sequence in sequencer.sequences:
 
-                # Check if sequence should be checked out
+                # Check if directory has been overriden
                 #
-                exportPath = sequence.exportPath()
-                requiresAdding = self.checkout and not os.path.exists(exportPath)
+                if not stringutils.isNullOrEmpty(self.alternateDirectory):
 
-                if self.checkout and not requiresAdding:
-
-                    cmds.edit(exportPath)
+                    sequence.directory = self.alternateDirectory
 
                 # Export sequence
                 #
-                sequence.export()
-
-                # Check if sequence should be added
-                #
-                if requiresAdding:
-
-                    cmds.add(exportPath)
+                exportPath = sequence.export(checkout=self.checkout)
 
     def doIt(self, *args, **kwargs):
         """
